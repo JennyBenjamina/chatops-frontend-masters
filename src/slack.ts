04695154721bc4f294343fcdda2514e82b77254b
Jenny Lee <jennybenjamina@g.ucla.edu>
2023-09-02 06:54:15 -0700

    slack and util

--- src/slack.ts
+++ src/slack.ts
@@ -1,14 +1,52 @@
 import type { Handler } from '@netlify/functions';
+import { parse } from 'querystring';
+import { slackApi } from './util/slack';
 
-export const handler: Handler = async () => {
-	// TODO validate the Slack request
+async function handleSlashCommand(payload: SlackSlashCommandPayload) {
+  switch (payload.command) {
+    case '/foodfight':
+      const joke = await fetch('https://icanhazdadjoke.com', {
+        headers: {
+          accept: 'text/plain',
+        },
+      });
+      const response = await slackApi('chat.postMessage', {
+        channel: payload.channel_id,
+        text: await joke.text(),
+      });
 
-	// TODO handle slash commands
+      if (!response.ok) {
+        console.log(response);
+      }
 
-	// TODO handle interactivity (e.g. context commands, modals)
+      break;
 
-	return {
-		statusCode: 200,
-		body: 'TODO: handle Slack commands and interactivity',
-	};
+    default:
+      return {
+        statusCode: 200,
+        body: `Command ${payload.command} not supported`,
+      };
+  }
+  return {
+    statusCode: 200,
+    body: '',
+  };
+}
+
+export const handler: Handler = async (event) => {
+  // TODO validate the Slack request
+
+  // TODO handle slash commands
+  const body = parse(event.body ?? '') as SlackPayload;
+
+  if (body.command) {
+    return handleSlashCommand(body as SlackSlashCommandPayload);
+  }
+
+  // TODO handle interactivity (e.g. context commands, modals)
+
+  return {
+    statusCode: 200,
+    body: 'TODO: handle Slack commands and interactivity',
+  };
 };
